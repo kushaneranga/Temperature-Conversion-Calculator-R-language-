76d03632c9adb00377d0669b19e56989d06f13e1
Kushan Eranga <61194721+kushaneranga@users.noreply.github.com>
2020-11-13 17:17:25 +0530

    Add files via upload
    Temperature conversions with R language

---
+++ Temperature_R.R
@@ -0,0 +1,15 @@
+cat("- Converting the Fahrenheit Celsius -
+Convert Celsius to Fahrenheit: type-F
+Convert Fahrenheit to Celsius: type-C\n") #Concatenate
+inp <- toupper(readline(prompt = "What do you want: "))
+if (inp == "F"){
+  fahrenheit <- as.numeric(readline(prompt = "Enter the Temperature in Celsius: "))
+  f <- (fahrenheit * 1.8) + 32
+  cat("Fahrenheit: ",f)
+}else if (inp == "C"){
+  celsius <- as.numeric(readline(prompt = "Enter the Temperature in fahrenheit: "))
+  c <- (celsius - 32) * 5 / 9
+  cat("Celsius: ",c)
+}else{
+  noquote("Please check the character you entered(Also F or C)")
+}
